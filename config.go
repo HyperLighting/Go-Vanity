0f6f931d0b8319b845c94723decf70721e6d9a80
Joe Izzard <Joe@Izzard.me>
2021-08-05 19:08:03 +0100

    Added main structs for config

---
+++ config.go
@@ -0,0 +1,37 @@
+package main
+
+type Conf struct {
+	Server   Server
+	Projects ProjectSettings
+	Logging  Logging
+}
+
+type Server struct {
+	Port      int
+	Hostname  string
+	UseSSL    bool
+	StaticDir string
+}
+
+type ProjectSettings struct {
+	MetaRefresh MetaRefresh
+	Remote      RemoteProject
+}
+
+type MetaRefresh struct {
+	Enabled bool
+	To      string
+}
+
+type RemoteProject struct {
+	Enabled          bool
+	RefreshFrequency string
+	URL              string
+}
+
+type Logging struct {
+	Method string
+	Format string
+	Level  string
+	File   string
+}
