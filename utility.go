c341eb8d4a0c792742e9c9e1aac22639a41fce50
Joe Izzard <Joe@Izzard.me>
2021-08-15 04:32:53 +0100

    feat: added utility function to read file

---
+++ utility.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func readFile(fileName string) (out []byte, err error) {
+	// Open the File
+	file, errorOpen := os.Open(fileName)
+
+	// Error openning file?
+	if errorOpen != nil {
+		log.WithFields(log.Fields{
+			"File": fileName,
+		}).Error(errorOpen)
+		return nil, errorOpen
+	}
+
+	// Convert to Byte Array
+	byteValue, errorConvert := ioutil.ReadAll(file)
+
+	// Error Converting?
+	if errorConvert != nil {
+		log.WithFields(log.Fields{
+			"File": fileName,
+		}).Error(errorConvert)
+		return nil, errorConvert
+	}
+
+	return byteValue, nil
+}
