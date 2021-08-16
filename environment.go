93d803533bd1146f8ac9514a366ef24113a46960
Joe Izzard <Joe@Izzard.me>
2021-08-16 19:24:47 +0100

    refactor: removed config related environment variables
    Removed environment variables that are related to configuration of system, these environment variables will now be handled by config system.

--- environment.go
+++ environment.go
@@ -18,20 +18,6 @@ func initEnvironment() Env {
 		"ENVIRONMENT",
 		"NO_CONFIG_FILE",
 		"CONFIG_FILE",
-		"PROJECTS_FILE",
-		"PORT",
-		"STATIC_DIR",
-		"BASEURL",
-		"USESSL",
-		"METAREFRESH",
-		"METAREFRESH_TO",
-		"LOG_METHOD",
-		"LOG_FORMAT",
-		"LOG_LEVEL",
-		"LOG_FILE",
-		"FETCH_PROJECT_ENABLED",
-		"FETCH_PROJECT_URL",
-		"FETCH_PROJECT_FREQUENCY",
 	}
 
 	// Loop through all variables and build a map for use later
