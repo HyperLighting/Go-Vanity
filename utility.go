5d244b93934868783003ebea9e71f85d1f5ed6b0
Joe Izzard <Joe@Izzard.me>
2021-08-17 22:16:05 +0100

    feat: added func to read remote file

--- utility.go
+++ utility.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"io/ioutil"
+	"net/http"
 	"os"
 
 	log "github.com/sirupsen/logrus"
@@ -26,6 +28,35 @@ func readLocalFile(fileName string) (out []byte, err error) {
 	if errorConvert != nil {
 		log.WithFields(log.Fields{
 			"File": fileName,
+		}).Error(errorConvert)
+		return nil, errorConvert
+	}
+
+	return byteValue, nil
+}
+
+func readRemoteFile(source string) (out []byte, err error) {
+	// Get response from the URL
+	resp, err := http.Get(source)
+
+	if err != nil {
+		return out, err
+	}
+
+	// Defer Closing
+	defer resp.Body.Close()
+
+	// Check the status code
+	if resp.StatusCode != http.StatusOK {
+		return out, errors.New("status code not ok")
+	}
+
+	// Convert response to a byte array
+	byteValue, errorConvert := ioutil.ReadAll(resp.Body)
+
+	if errorConvert != nil {
+		log.WithFields(log.Fields{
+			"Source": source,
 		}).Error(errorConvert)
 		return nil, errorConvert
 	}
